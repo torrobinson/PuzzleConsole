857488c59dce5e201c00f8493fcbb2431b75d465
torrobinson <torrobinson@gmail.com>
2015-10-23 22:15:38 -0600

    Missed file
    missed file

--- PuzzleConsole/Program.cs
+++ PuzzleConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using PuzzleConsole.WorldTypes;
+﻿using PuzzleConsole.ActorTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,34 +11,38 @@ namespace PuzzleConsole
     {
         static void Main(string[] args)
         {
-            //Prep world and view (of the same size)
+            //Prepare the 3 layers of objects/actors we want in play
+            ActorLayer foreground = new ActorLayer("Foreground stuff",                            1);
+            foreground.InitializeFromFile("Maps/foreground.txt");
+            foreground.Visible = false;
 
-            ActorLayer foreground = new ActorLayer(1, 1, 1);
-            foreground.InitializeFromFile("foreground.txt");
+            ActorLayer wallsAndItems = new ActorLayer("Main layer with players, walls, and items",0);
+            wallsAndItems.InitializeFromFile("Maps/walls_and_items.txt");
+            wallsAndItems.Visible = false;
 
-            ActorLayer wallsAndItems = new ActorLayer(1,1,0);
-            wallsAndItems.InitializeFromFile("walls_and_items.txt");
-
-            ActorLayer background = new ActorLayer(1, 1,-1);
-            background.InitializeFromFile("background.txt");
-
-
+            ActorLayer background = new ActorLayer("Background stuff",                           -1);
+            background.InitializeFromFile("Maps/background.txt");
 
             List<ActorLayer> layersToRender = new List<ActorLayer>(){foreground, wallsAndItems, background };
 
-            //Now we have a wallsAndItems layer and a background layer below
 
             //Create a viewport sized for this map
-            Viewport view = new Viewport(wallsAndItems.Height, wallsAndItems.Width);
+            //Viewport view = new Viewport(wallsAndItems.Height, wallsAndItems.Width);
+            Viewport view = new Viewport(15, 30);
 
             //Fetch the first player in the map and bind controls to them (the player was automatically added from being read in from the map)
             Player thePlayer = (Player)wallsAndItems.FindFirstObjectInWorldOfType(typeof(Player));
-            
-            //Loop forever and ever
-            while (true) {
-                //Render the "frame"
-                view.RenderWorld(layersToRender);
 
+            //Set the viewports camera location to the player
+            view.CameraLocation = thePlayer.Location;
+            
+            //Loop and wait for input
+            while (true) {
+
+                //Render the frame using the layers we've defined
+                view.RenderFrame(layersToRender);
+
+                //Pause and capture movements
                 switch (Console.ReadKey(false).Key)
                 {
                     case ConsoleKey.UpArrow:
@@ -55,7 +59,15 @@ namespace PuzzleConsole
                         break;
                 }
 
-                System.Threading.Thread.Sleep(10);
+                //After the movement, update the camera location before the next frame render
+                view.CameraLocation = thePlayer.Location;
+
+                //Reinforce the window size in case it was changed
+                view.RefreshSize();
+
+                //If the user tytped any printable characters, this will keep them from writing over the world
+                // by jumping the cursor back
+                Console.Write("\b \b");
             }
             
         }
